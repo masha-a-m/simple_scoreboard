b37ef446eef07f32b162daea1b26334d194924d8
Maryam Garba <106096238+masha-a-m@users.noreply.github.com>
2024-07-27 03:55:24 +0800

    more commits

--- index.js
+++ index.js
@@ -1,76 +1,131 @@
-// Home screen
-
-let screenEl = document.getElementById("screen");
+// Home Screen
+let addBtn = document.getElementById("screen");
 
 let addOne = 0;
 
 function plusOne() {
     addOne += 1;
-    console.log(addOne);
-    screenEl.textContent = addOne;
+    addBtn.textContent = addOne;
 }
-
-
-let addTwo = 0;
-
 function plusTwo() {
-    addTwo += 2;
-    console.log(addTwo);
-
-    screenEl.textContent = addTwo;
+    addOne += 2;
+    addBtn.textContent = addOne;
 }
-
-
-let addThree = 0;
-
 function plusThree() {
-    addThree += 3;
-    console.log(addThree);
-
-    screenEl.textContent = addThree;
+    addOne += 3;
+    addBtn.textContent = addOne;
 }
-
-
 function resetHome() {
-    screenEl.textContent = 0;
+    addOne = 0;
+    addBtn.textContent = 0;
 }
 
 
-
-
-
-
-//Guest screen
-let screenGuestEL = document.getElementById("screenGuest");
-
-let addOneGuest = 0;
+// Guest Screen
+let addGuestBtn = document.getElementById("screenGuest");
 
 function plusOneGuest() {
-    addOneGuest += 1;
-    console.log(addOneGuest);
-    screenGuestEL.textContent = addOneGuest;
+    addOne += 1;
+    addGuestBtn.textContent = addOne;
 }
-
-
-let addTwoGuest = 0;
-
 function plusTwoGuest() {
-    addTwoGuest += 2;
-    console.log(addTwoGuest);
-
-    screenGuestEL.textContent = addTwoGuest;
+    addOne += 2;
+    addGuestBtn.textContent = addOne;
 }
-
-
-let addThreeGuest = 0;
-
 function plusThreeGuest() {
-    addThreeGuest += 3;
-    console.log(addThreeGuest);
-
-    screenGuestEL.textContent = addThreeGuest;
+    addOne += 3;
+    addGuestBtn.textContent = addOne;
 }
-
 function resetGuest() {
-    screenGuestEL.textContent = 0;
+    addOne = 0;
+    addGuestBtn.textContent = 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//works but the btns when clicked add differently
+// Home screen
+
+// let screenEl = document.getElementById("screen");
+
+// let addOne = 0;
+
+// function plusOne() {
+//     addOne += 1;
+//     console.log(addOne);
+//     screenEl.textContent = addOne;
+
+// }
+
+// let addTwo = 0;
+
+// function plusTwo() {
+//     addTwo += 2;
+//     console.log(addTwo);
+
+//     screenEl.textContent = addTwo;
+// }
+
+// let addThree = 0;
+
+// function plusThree() {
+//     addThree += 3;
+//     console.log(addThree);
+
+//     screenEl.textContent = addThree;
+// }
+
+// function resetHome() {
+//     screenEl.textContent = 0;
+// }
+
+
+
+
+
+
+// //Guest screen
+// let screenGuestEL = document.getElementById("screenGuest");
+
+// let addOneGuest = 0;
+
+// function plusOneGuest() {
+//     addOneGuest += 1;
+//     console.log(addOneGuest);
+//     screenGuestEL.textContent = addOneGuest;
+// }
+
+
+// let addTwoGuest = 0;
+
+// function plusTwoGuest() {
+//     addTwoGuest += 2;
+//     console.log(addTwoGuest);
+
+//     screenGuestEL.textContent = addTwoGuest;
+// }
+
+
+// let addThreeGuest = 0;
+
+// function plusThreeGuest() {
+//     addThreeGuest += 3;
+//     console.log(addThreeGuest);
+
+//     screenGuestEL.textContent = addThreeGuest;
+// }
+
+// function resetGuest() {
+//     screenGuestEL.textContent = 0;
+// }
